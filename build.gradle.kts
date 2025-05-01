516448fa151ea4f8c7e3ba58b8204a4c53ccd933
quarkusio <no-reply@quarkus.io>
2025-05-01 21:26:33 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkiverse.roq:quarkus-roq:1.5.1")
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkiverse.roq:quarkus-roq-theme-default:1.5.1")
+    testImplementation("io.quarkus:quarkus-junit5")
+}
+
+group = "dev.suresh"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
